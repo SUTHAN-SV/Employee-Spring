254c2a6761658f27f77e9ad79764b8d289242c78
SUTHAN-SV <svsuthan64@gmail.com>
2026-07-14 10:43:13 +0530

    Upload Spring Boot project

---
+++ src/main/java/com/example/demo/EmployeeSpring1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSpring1Application.class, args);
+	}
+
+}
